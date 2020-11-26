fb99ec02d66641aa912b68c691502f0ac964bd76
Andre-MR-Pereira <andregdpereira@gmail.com>
2020-11-26 12:46:05 +0000

    BFV encryption example revamp

--- main.cpp
+++ main.cpp
@@ -114,5 +114,70 @@ int main()
     cout << endl;
     cout << "~~~~~~ A better way to calculate 4(x^2+1)(x+1)^2. ~~~~~~" << endl;
 
+    //Aplicacao realinearizacao
+    print_line(__LINE__);
+    cout << "Generate relinearization keys." << endl;
+    RelinKeys relin_keys;
+    keygen.create_relin_keys(relin_keys);
+
+    //repeticao calculos co realinearizacao
+    print_line(__LINE__);
+    cout << "Compute and relinearize x_squared (x^2)," << endl;
+    cout << string(13, ' ') << "then compute x_sq_plus_one (x^2+1)" << endl;
+    Ciphertext x_squared;
+    evaluator.square(x_encrypted, x_squared);
+    cout << "    + size of x_squared: " << x_squared.size() << endl;
+    evaluator.relinearize_inplace(x_squared, relin_keys);
+    cout << "    + size of x_squared (after relinearization): " << x_squared.size() << endl;
+    evaluator.add_plain(x_squared, plain_one, x_sq_plus_one);
+    cout << "    + noise budget in x_sq_plus_one: " << decryptor.invariant_noise_budget(x_sq_plus_one) << " bits"
+         << endl;
+    cout << "    + decryption of x_sq_plus_one: ";
+    decryptor.decrypt(x_sq_plus_one, decrypted_result);
+    cout << "0x" << decrypted_result.to_string() << " ...... Correct." << endl;
+
+    print_line(__LINE__);
+    Ciphertext x_plus_one;
+    cout << "Compute x_plus_one (x+1)," << endl;
+    cout << string(13, ' ') << "then compute and relinearize x_plus_one_sq ((x+1)^2)." << endl;
+    evaluator.add_plain(x_encrypted, plain_one, x_plus_one);
+    evaluator.square(x_plus_one, x_plus_one_sq);
+    cout << "    + size of x_plus_one_sq: " << x_plus_one_sq.size() << endl;
+    evaluator.relinearize_inplace(x_plus_one_sq, relin_keys);
+    cout << "    + noise budget in x_plus_one_sq: " << decryptor.invariant_noise_budget(x_plus_one_sq) << " bits"
+         << endl;
+    cout << "    + decryption of x_plus_one_sq: ";
+    decryptor.decrypt(x_plus_one_sq, decrypted_result);
+    cout << "0x" << decrypted_result.to_string() << " ...... Correct." << endl;
+
+    print_line(__LINE__);
+    cout << "Compute and relinearize encrypted_result (4(x^2+1)(x+1)^2)." << endl;
+    evaluator.multiply_plain_inplace(x_sq_plus_one, plain_four);
+    evaluator.multiply(x_sq_plus_one, x_plus_one_sq, encrypted_result);
+    cout << "    + size of encrypted_result: " << encrypted_result.size() << endl;
+    evaluator.relinearize_inplace(encrypted_result, relin_keys);
+    cout << "    + size of encrypted_result (after relinearization): " << encrypted_result.size() << endl;
+    cout << "    + noise budget in encrypted_result: " << decryptor.invariant_noise_budget(encrypted_result) << " bits"
+         << endl;
+
+    cout << endl;
+    cout << "NOTE: Notice the increase in remaining noise budget." << endl;
+
+    //Maior noise budge>provavelmente a decriptacao esta correta
+    print_line(__LINE__);
+    cout << "Decrypt encrypted_result (4(x^2+1)(x+1)^2)." << endl;
+    decryptor.decrypt(encrypted_result, decrypted_result);
+    cout << "    + decryption of 4(x^2+1)(x+1)^2 = 0x" << decrypted_result.to_string() << " ...... Correct." << endl;
+    cout << endl;
+
+    //Extra:Microsoft SEAL consegue dizer porque os parametros estao errados.
+    //Neste caso, com o poly de 2048 o maximo do modulo dos coefficients so pode ir ate 54
+    print_line(__LINE__);
+    cout << "An example of invalid parameters" << endl;
+    parms.set_poly_modulus_degree(2048);
+    context = SEALContext(parms);
+    print_parameters(context);
+    cout << "Parameter validation (failed): " << context.parameter_error_message() << endl << endl;
+
     return 0;
 }
